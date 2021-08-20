5f248b0e8ca22d998262d8d4ef97a630eae0c79f
sharunerajeev <sharunpublic@gmail.com>
2021-08-20 22:14:40 +0530

    Add Day 17

---
+++ 17/class_with_constructor.py
@@ -0,0 +1,9 @@
+class Car:
+    # Initialising attributes for the class Car
+    def __init__(self):
+        self.speed = 200
+        self.id = 1001
+
+
+my_car = Car()
+print(my_car.id, my_car.speed)
